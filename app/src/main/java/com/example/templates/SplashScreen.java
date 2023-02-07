a4dfa5a423e23c94cc9fa951481f51cd475d508d
Ayusi-Peter2 <kevinwafula716@gmail.com>
2023-02-07 17:38:50 +0300

    Update style on splash and main activity

--- app/src/main/java/com/example/templates/SplashScreen.java
+++ app/src/main/java/com/example/templates/SplashScreen.java
@@ -2,13 +2,48 @@ package com.example.templates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView txt;
+    ImageView bg,logo;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler h = new Handler();
+
+        txt = findViewById(R.id.textView);
+        //bg= findViewById(R.id.imageViewSplash);
+        logo = findViewById(R.id.logo);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        txt.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        //bg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
+
+
     }
 }
